5aaa5b4a95de21d20bcf2b28b8c7d8c9a124ab00
OrhanBTN <87336665+OrhanBTN@users.noreply.github.com>
2022-09-12 12:18:32 +0300

    otovoid

--- otovoid.sh
+++ otovoid.sh
@@ -4,26 +4,26 @@ echo "Void Linux Otomatik Yukleyci\n"
 echo "By OrhanBTN\n"
 echo "Yukleme Baslıyor Iptal Etmek Icın Ctrl + C Basın\n"
     echo "5..."
-    sleep 1s
+    sleep 5
     echo "4..."
-    sleep 1
+    sleep 5
     echo "3..."
-    sleep 1
+    sleep 5
     echo "2..."
-    sleep 1
+    sleep 5
     echo "1..."
-    sleep 1
+    sleep 5
     echo "0..."
     cls 
 echo "Depo ve Sistem Guncellemsi Basliyor...\n"
     xbps-install -Suy
     echo "Tamam!"
-    sleep 1
+    sleep 5
     cls
 echo "Gerekli Paketler Yukleniyor...\n"
     xbps-install -y curl wget unzip zip nano vim gptfdisk mtools mlocate ntfs-3g fuse-exfat bash-completion
     echo "Tamam!"
-    sleep 1
+    sleep 5
     cls
 echo "X Pencere Sistemi Yukleniyor...\n"
     xbps-install -y xorg-server xorg-server-xwayland xorg-video-drivers xorg-input-drivers xinit xauth xrandr xrdb xwininfo xdpyinfo xsetroot neofetch
