e2b2cbc0185d396c7cb68c6fd1130ce967dca4ff
OrhanBTN <87336665+OrhanBTN@users.noreply.github.com>
2022-09-12 12:13:34 +0300

    otovoid

--- otovoid.sh
+++ otovoid.sh
@@ -1,29 +1,29 @@
 alias cls="clear"
 cls
-echo -e "Void Linux Otomatik Yukleyci\n"
-echo -e "By OrhanBTN\n"
-echo -e "Yukleme Baslıyor Iptal Etmek Icın Ctrl + C Basın\n"
-    echo -e "5..."
+echo "Void Linux Otomatik Yukleyci\n"
+echo "By OrhanBTN\n"
+echo "Yukleme Baslıyor Iptal Etmek Icın Ctrl + C Basın\n"
+    echo "5..."
     sleep 1s
-    echo -e "4..."
+    echo "4..."
     sleep 1
-    echo -e "3..."
+    echo "3..."
     sleep 1
-    echo -e "2..."
+    echo "2..."
     sleep 1
-    echo -e "1..."
+    echo "1..."
     sleep 1
-    echo -e "0..."
+    echo "0..."
     cls 
-echo -e "Depo ve Sistem Guncellemsi Basliyor...\n"
+echo "Depo ve Sistem Guncellemsi Basliyor...\n"
     xbps-install -Suy
-    echo -e "Tamam!"
+    echo "Tamam!"
     sleep 1
     cls
-echo -e "Gerekli Paketler Yukleniyor...\n"
-    xbps-install curl wget unzip zip nano vim gptfdisk mtools mlocate ntfs-3g fuse-exfat bash-completion
-    echo -e "Tamam!"
+echo "Gerekli Paketler Yukleniyor...\n"
+    xbps-install -y curl wget unzip zip nano vim gptfdisk mtools mlocate ntfs-3g fuse-exfat bash-completion
+    echo "Tamam!"
     sleep 1
     cls
-echo -e "X Pencere Sistemi Yukleniyor...\n"
-    xbps-install xorg-server xorg-server-xwayland xorg-video-drivers xorg-input-drivers xinit xauth xrandr xrdb xwininfo xdpyinfo xsetroot neofetch
+echo "X Pencere Sistemi Yukleniyor...\n"
+    xbps-install -y xorg-server xorg-server-xwayland xorg-video-drivers xorg-input-drivers xinit xauth xrandr xrdb xwininfo xdpyinfo xsetroot neofetch
